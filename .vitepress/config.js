608279021ca553211cb8907d2f49c218bedf65fb
Ripper <pleshu.andrew@lll.kpi.ua>
2025-03-04 20:26:51 +0200

    Виправлення

---
+++ .vitepress/config.js
@@ -0,0 +1,18 @@
+export default {
+    title: "FlexForm",
+    themeConfig: {
+        nav: [
+            { text: "Головна", link: "/" },
+            { text: "Зміст", link: "/zmist" },
+            { text: "Автори", link: "/autors" },
+        ],
+        sidebar: [
+            { text: 'Головна', link: '/' },
+            { text: 'Зміст', link: '/zmist' },
+            { text: 'Аналіз предметної області', link: '/analysis' },
+            { text: 'Призначення розроблюваної системи', link: '/appointment' },
+            { text: 'Аналіз зацікавлених осіб для системи організації та управління опитуваннями експертів', link: '/interested_parties' },
+            { text: 'Автори', link: '/autors' },
+        ],
+    },
+};
